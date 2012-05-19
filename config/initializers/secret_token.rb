130b7db5fa6720ec57ac14c1af7821b2555ca98c
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2012-05-19 00:19:09 -0300

    Site pousada paraiso, em rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pousadaparaiso::Application.config.secret_token = '6be570f1268c8cc1ddbba9c211bcc973b3815bdef37b6e89b4fa8be18b92af7d16a1682d0e8ec24f32f3c953e5d532539148279fb7ed21fc37eba4abecbf9545'
