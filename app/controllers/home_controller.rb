d3083d5af92b9ebcbf1d99094324583473e976ae
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2012-08-13 19:08:05 -0300

    atualização

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-    
+    @galleries = Gallery.all
   end
 end
---
+++ app/models/gallery.rb
@@ -0,0 +1,3 @@
+class Gallery < ActiveRecord::Base
+  attr_accessible :data_cadastro, :titulo
+end
